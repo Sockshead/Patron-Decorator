15d9bf6e9fe799a01df3af89b534063bdb41b0f4
Sockshead <juancapoar@gmail.com>
2018-09-16 20:34:27 -0500

    Menu fix

--- taller2PatronDecorator/src/taller2patrondecorator/Taller2PatronDecorator.java
+++ taller2PatronDecorator/src/taller2patrondecorator/Taller2PatronDecorator.java
@@ -1,90 +1,122 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package taller2patrondecorator;
 
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author JUan Camilo Posada
- */
 public class Taller2PatronDecorator {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        ArrayList<component> pagos = new ArrayList();
+    //variables de clase
+    private ArrayList<component> pagos;
+    String usuario;
+
+    //Métodos
+    public static void main(String[] xxx) {
+        new Taller2PatronDecorator();
+    }
+
+    public Taller2PatronDecorator() {
+        this.pagos = new ArrayList();
+        this.usuario = "";
+        this.menu();
+    }
+
+    private void menu() {
+        char opcion;
+        do {
+            this.usuario = JOptionPane.showInputDialog("Ingrese su numero de cedula");
+            String x = JOptionPane.showInputDialog("=======OPCIONES=========\n"
+                    + "1. Realizar Pago \n"
+                    + "2. Ver historial de pagos \n"
+                    + "0. SALIR "
+            );
+            opcion = x.charAt(0);
+            switch (opcion) {
+                case '1':
+                    this.rPago();
+                    break;
+                case '2':
+                    if (!this.pagos.isEmpty()) {
+                        this.verPagos();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Regresa aqui despues de realizar tu primer pago!", "Error", 0);
+                    }
+                    break;
+                case '0':
+                    JOptionPane.showMessageDialog(null, "CHAO PESCAO..!", "Despedida", 2);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "ESE CASO NO EXISTE..!", "Error", 0);
+                    break;
+            }
+        } while (opcion != '0');
+    }
+
+    private void rPago() {
         Random ran = new Random();
         component com;
-        String usuario = "";
-        try {
-            usuario = JOptionPane.showInputDialog("Ingrese su numero de cedula");
-            int entrada = Integer.parseInt(JOptionPane.showInputDialog("1. Pagar \n 2. Ver Pagos \n 0. Salir"));
+        char opcion;
+        do {
+            String x = JOptionPane.showInputDialog("=======OPCIONES=========\n"
+                    + "1. Pagar via Baloto \n"
+                    + "2. Pagar en Efectivo \n"
+                    + "3. Pagar via Credito \n"
+                    + "0. Regresar "
+            );
+            opcion = x.charAt(0);
+            switch (opcion) {
+                case '1':
+                    com = new Baloto(new Pago());
+                    int entrada2 = Integer.parseInt(JOptionPane.showInputDialog("1. Asignar valor a pagar \n 2. Regresar "));
+                    switch (entrada2) {
+                        case 1:
+                            String Valor = JOptionPane.showInputDialog("Ingrese valor a pagar: ");
+                            com.asignarValores(Valor, "" + (ran.nextInt(9000000) + 1000000), usuario);
+                            pagos.add(com);
+                            JOptionPane.showMessageDialog(null, com.mostrarValores());
+                            break;
+                    }
+                    break;
+                case '2':
+                    com = new Efectivo(new Pago());
+                    entrada2 = Integer.parseInt(JOptionPane.showInputDialog("1. Asignar valor a pagar \n 2. Regresar "));
+                    switch (entrada2) {
+                        case 1:
+                            String Valor = JOptionPane.showInputDialog("Ingrese valor a pagar: ");
+                            com.asignarValores(Valor, "" + (ran.nextInt(9000000) + 1000000), usuario);
+                            pagos.add(com);
+                            JOptionPane.showMessageDialog(null, com.mostrarValores());
+                            break;
+                    }
+                    break;
+                case '3':
+                    com = new Credito(new Pago());
+                    entrada2 = Integer.parseInt(JOptionPane.showInputDialog("1. Asignar valor a pagar \n 2. Regresar "));
+                    switch (entrada2) {
+                        case 1:
+                            String Valor = JOptionPane.showInputDialog("Ingrese valor a pagar: ");
+                            com.asignarValores(Valor, "" + (ran.nextInt(9000000) + 1000000), usuario);
+                            pagos.add(com);
+                            JOptionPane.showMessageDialog(null, com.mostrarValores());
+                            break;
+                    }
+                    break;
+                case '0':
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "ESE CASO NO EXISTE..!", "Error", 0);
+                    break;
+            }
+        } while (opcion != '0');
+    }
 
-            do {
-                switch (entrada) {
-                    case 1:
-                        int entrada1 = Integer.parseInt(JOptionPane.showInputDialog("1. Pagar via Baloto \n 2. Pagar en Efectivo \n 3.Pagar via Credito \n 4.Regresar"));
-                        switch (entrada1) {
-                            case 1:
-                                com = new Baloto(new Pago());
-                                int entrada2 = Integer.parseInt(JOptionPane.showInputDialog("1. Asignar valor a pagar \n 2. Regresar "));
-                                switch (entrada2) {
-                                    case 1:
-                                        String Valor = JOptionPane.showInputDialog("Ingrese valor a pagar: ");
-                                        com.asignarValores(Valor, "" + (ran.nextInt(9000000) + 1000000), usuario);
-                                        pagos.add(com);
-                                        JOptionPane.showMessageDialog(null, com.mostrarValores());
-                                        break;
-                                }
-                                break;
-                            case 2:
-                                com = new Efectivo(new Pago());
-                                entrada2 = Integer.parseInt(JOptionPane.showInputDialog("1. Asignar valor a pagar \n 2. Regresar "));
-                                switch (entrada2) {
-                                    case 1:
-                                        String Valor = JOptionPane.showInputDialog("Ingrese valor a pagar: ");
-                                        com.asignarValores(Valor, "" + (ran.nextInt(9000000) + 1000000), usuario);
-                                        pagos.add(com);
-                                        JOptionPane.showMessageDialog(null, com.mostrarValores());
-                                        break;
-                                }
-                                break;
-                            case 3:
-                                com = new Credito(new Pago());
-                                entrada2 = Integer.parseInt(JOptionPane.showInputDialog("1. Asignar valor a pagar \n 2. Regresar "));
-                                switch (entrada2) {
-                                    case 1:
-                                        String Valor = JOptionPane.showInputDialog("Ingrese valor a pagar: ");
-                                        com.asignarValores(Valor, "" + (ran.nextInt(9000000) + 1000000), usuario);
-                                        pagos.add(com);
-                                        JOptionPane.showMessageDialog(null, com.mostrarValores());
-                                        break;
-                                }
-                                break;
-
-                        }
-                        break;
-                    case 2:
-                        String historial = "";
-                        for (component pago : pagos) {
-                            if (pago.darUsuario().equalsIgnoreCase(usuario)) {
-                                historial = historial + pago.mostrarValores() + "\n";
-                            }
-                        }
-                        JOptionPane.showMessageDialog(null, historial);
-                        break;
-                }
-            } while ((0 <= entrada && entrada <= 3));
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Ingrese una opcion Valida");
+    private void verPagos() {
+        String historial = "Historial de pagos: \n";
+        for (component pago : pagos) {
+            if (pago.darUsuario().equalsIgnoreCase(usuario)) {
+                historial = historial + pago.mostrarValores() + "\n";
+            }
         }
+        JOptionPane.showMessageDialog(null, historial);
     }
 }
