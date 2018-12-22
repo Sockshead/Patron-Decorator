6ba8ae6c2893fbfc7c1b2575db8539e05eaec405
Sockshead <juancapoar@gmail.com>
2018-12-22 00:30:48 -0500

    Update MediosDePago.java

--- taller2PatronDecorator/src/taller2patrondecorator/MediosDePago.java
+++ taller2PatronDecorator/src/taller2patrondecorator/MediosDePago.java
@@ -1,25 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package taller2patrondecorator;
 
-/**
- *
- * @author JUan Camilo Posada
- */
 abstract class MediosDePago implements component {
+
     protected component mediosPagopse;
-    protected String mediopag="";
-    
-    public MediosDePago(component pMediosPago){
-        mediosPagopse=pMediosPago;
+    protected String mediopag = "";
+
+    public MediosDePago(component pMediosPago) {
+        mediosPagopse = pMediosPago;
     }
-    public String mostrarValores(){
-       return this.mediosPagopse.mostrarValores()+"Pago realizado por: "+mediopag;
+
+    public String mostrarValores() {
+        return this.mediosPagopse.mostrarValores() + " Pago realizado por: " + mediopag;
     }
-    public void asignarValores(String pPago){
-        this.mediopag=pPago;
+
+    public void asignarValores(String pPago) {
+        this.mediopag = pPago;
     }
 }
